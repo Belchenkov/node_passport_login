b84bd4c6d12fd804eebb335622ba9144e798c4e3
Алексей Бельченков <u608110@gmail.com>
2019-01-14 12:43:16 +0400

    Write Register Validator

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,25 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
+// Register Handle
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    // Check required fields
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please fill in all fields' });
+    }
+
+    // Check passwords match
+    if (password !== password2) {
+        errors.push({ msg: 'Passwords do not match' });
+    }
+
+    // Check pass length
+    if (password.length < 6) {
+        errors.push({ msg: 'Passwords should be at least 6 characters' });
+    }
+});
+
 module.exports = router;

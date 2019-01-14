3114fe3452e2bf376a6ed323988313c3f40ebcbb
Алексей Бельченков <u608110@gmail.com>
2019-01-14 15:07:36 +0400

    Logout

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+// Welcome Page
 router.get('/', (req, res) => {
     res.render('welcome');
+});
+
+// Dashboard Page
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard');
 });
 
 module.exports = router;

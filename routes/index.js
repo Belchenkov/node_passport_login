9626ec16a9edb1c8254642a38529ba97de283826
Алексей Бельченков <u608110@gmail.com>
2019-01-14 15:17:37 +0400

    Guard for Dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
 // Welcome Page
 router.get('/', (req, res) => {
@@ -7,8 +8,10 @@ router.get('/', (req, res) => {
 });
 
 // Dashboard Page
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard');
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+    res.render('dashboard', {
+        name: req.user.name
+    });
 });
 
 module.exports = router;

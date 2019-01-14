790f003a5d56335b89954140b18344383eb98441
Алексей Бельченков <u608110@gmail.com>
2019-01-14 13:18:37 +0400

    Generate Hash Password && Save New Users

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+// User Model
+const User = require('../models/User');
 
 // Login Page
 router.get('/login', (req, res) => {
@@ -40,7 +44,42 @@ router.post('/register', (req, res) => {
             password2
         });
     } else {
-        res.send('pass');
+        // Validation passed
+        User.findOne({ email: email })
+            .then(user => {
+                if (user) {
+                    // User Exists
+                    errors.push({ msg: 'Email is already registered' });
+                    res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    });
+                } else {
+                    // Create new User
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    });
+
+                    // Hash Password
+                    bcrypt.genSalt(10, (err, salt) => bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        // Set password to hashed
+                        newUser.password = hash;
+                        // Save user
+                        newUser.save()
+                            .then(user => {
+                                res.redirect('/users/login');
+                            })
+                            .catch(err => console.log(err));
+
+                    }));
+                }
+            });
     }
 });
 
